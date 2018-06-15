037e7d3b90936a1f9d4ed63af53080b534a0466d
jiro.kirameki <saeko.ueno@techacademy.jp>
2018-06-15 06:32:13 +0000

    fix

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -22,6 +22,7 @@ class MicropostsController extends Controller
             ];
             
             $data += $this->counts($user);
+            
             return view('users.show', $data);
         }
         
